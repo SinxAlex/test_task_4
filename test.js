22327badace4716b9017134da4ed1730a2b62356
Alex <aleshaascet@gmail.com>
2025-05-16 01:27:38 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,48 @@
+widgetNameIntr = function() {
+    var widget = this;
+    this.code = null;
+
+    this.yourVar = {};
+    this.yourFunc = function() {};
+
+    // вызывается один раз при инициализации виджета, в этой функции мы вешаем события на $(document)
+    this.bind_actions = function(){
+
+        const div = document.getElementById("status_id_42497374");
+        div.style.color = "blue";
+        div.style.backgroundColor = "yellow";
+        div.style.fontSize = "24px";
+        div.style.padding = "10px";
+    };
+
+
+    this.render = function() {
+        console.log('render');
+    };
+
+    // вызывается один раз при инициализации виджета, в этой функции мы загружаем нужные данные, стили и.т.п
+    this.init = function(){
+
+    };
+
+    // метод загрузчик, не изменяется
+    this.bootstrap = function(code) {
+        widget.code = code;
+        // если frontend_status не задан, то считаем что виджет выключен
+        // var status = yadroFunctions.getSettings(code).frontend_status;
+        var status = 1;
+
+        if (status) {
+            widget.init();
+            widget.render();
+            widget.bind_actions();
+            $(document).on('widgets:load', function () {
+                widget.render();
+            });
+        }
+    }
+};
+// создание экземпляра виджета и регистрация в системных переменных Yadra
+// widget-name - ИД и widgetNameIntr - уникальные названия виджета
+yadroWidget.widgets['widget-name'] = new widgetNameIntr();
+yadroWidget.widgets['widget-name'].bootstrap('widget-name');
